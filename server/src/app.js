a87777c8bf24917bb92f0030efd229e1cbb1cdbe
onubrooks <abahonuh@gmail.com>
2023-08-18 21:08:51 +0100

    add some CI/CD set up for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -6,7 +7,11 @@ const app = express();
 
 app.use(cors({ origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
